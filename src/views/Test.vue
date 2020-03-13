1aa7d7f52c4805e1ae5b41b1d0f10d6bcaf4fe7c
Allen Chen <allen.chen@hiero7.com>
2020-03-13 12:55:14 +0800

    add Test vue template

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,13 +1,30 @@
-<template>
-    
+<template  lang="pug">
+    v-container#sample-layout(grid-list-lg)
+        v-layout(wrap)
+            v-flex(xs12)
+                .title Test Tool
+            v-flex(xs12)
+                v-card
+                    v-card-text
+                        v-form(ref="form" onsubmit="return false;")
+                            v-layout(wrap)
 </template>
 
 <script>
+  import textFieldRules from "../utils/textFieldRules";
   export default {
-    name: "Test"
-  }
+    name: "Test",
+    mixins: [textFieldRules],
+    data() {
+      return {
+      }
+    },
+    methods: {
+
+    }
+  };
 </script>
 
-<style scoped>
+<style lang="sass" scoped>
 
 </style>
