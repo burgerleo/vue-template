417fbfa256672e63c862e64cd99db9e493f6af47
Allen Chen <allen.chen@hiero7.com>
2020-03-19 15:36:29 +0800

    add helper edit dialog

--- src/views/admin/Helper.vue
+++ src/views/admin/Helper.vue
@@ -5,7 +5,21 @@
                 v-layout.ml-1.mt-1(row)
                     .title {{ title }} Online Helper
                     v-spacer
-                    v-btn(color="primary" dark ) Edit
+                    v-dialog(v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition")
+                        template(v-slot:activator="{ on }")
+                            v-btn(color="primary"  dark v-on="on") Edit
+                        v-card
+                            v-toolbar(dark color="primary")
+                                v-btn(icon dark @click="dialog = false")
+                                    v-icon mdi-close
+                                v-toolbar-title Edit
+                                v-spacer
+                                v-toolbar-items
+                                    v-btn(dark text @click="dialog= false") Save
+                            v-list(three-line subheader)
+                                v-subheader Edit Helper Content
+                                    
+
             v-flex(xs12)
                 v-card
                     v-card-text
@@ -17,7 +31,11 @@
     mixins: [textFieldRules],
     data() {
       return {
-        title:''
+        title:'',
+        dialog: false,
+        notifications: false,
+        sound: true,
+        widgets: false,
       };
     },
     watch:{
