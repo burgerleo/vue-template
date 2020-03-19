ec19db82f90dd8e8d7bdf833cab1f568f3fe132f
lin.leo <lin.leo@hiero7.com>
2020-03-19 09:56:35 +0800

    add DataTableAction

---
+++ src/components/DataTableAction.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+    #dataTable
+        v-data-table.elevation-1(:headers="headers" :items="items" :page.sync="page" :items-per-page="itemsPerPage" :search="searchText" hide-default-footer @page-count="pageCount = $event" :dense="dense")
+            template(slot="items" slot-scope="props")
+                slot(name="items" :props="props")
+
+
+        v-row.align-center(v-if="hideFooter")
+            v-col.pa-5(cols="12" sm="4")
+                v-text-field(:value="itemsPerPage" label="Items per page" type="number" min="1" max="15" @input="itemsPerPage = parseInt($event, 10)")
+            v-col.pa-5(cols="12" sm="4")
+            v-col.pa-5(cols="12" sm="4")
+                v-pagination(v-model="page" :length="pageCount")
+</template>
+<script>
+export default {
+    props: {
+        headers: {
+            type: Array
+        },
+        items: {
+            type: Array
+        },
+        dense: {
+            type: Boolean,
+            default: true
+        },
+        hideFooter: {
+            type: Boolean,
+            default: true
+        },
+        searchText: {
+            type: String,
+            default: ''
+        }
+    },
+    data() {
+        return {
+            page: 1,
+            pageCount: 0,
+            itemsPerPage: 10
+        }
+    },
+    methods: {
+    }
+}
+</script>
+<style lang="sass" scoped>
+*
+    // border: 1px solid black
+</style>
+
