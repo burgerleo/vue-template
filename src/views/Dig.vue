31a2e30302deba77b47922bef750435580c05334
justin.hsu <justin.hsu@hiero7.com>
2020-05-08 17:38:09 +0800

    edit dig

--- src/views/Dig.vue
+++ src/views/Dig.vue
@@ -7,12 +7,18 @@
                         v-form(ref="form" onsubmit="return false;")
                             v-layout.px-2
                                 v-flex.py-6.pt-0.pb-0(xs12 sm12 md12)
-                                    v-text-field(v-model="destinationIP" label="Destination FQDN")
+                                    v-text-field(v-model="destinationIP" label="Destination FQDN" type="domain" :rules="[rules.required, rules.domain]")
+                                    v-text-field(v-model="dnsServerIP" label="DNS Server IP" type="ip" :rules="[rules.required, rules.ip]")
+                            v-layout.px-2
+                                v-flex.pl-2(xs1 sm1 md1) Options
+                                v-flex(xs12 sm12 md12)
+                                    v-radio-group(v-model='siteSelectedName' row)
+                                        v-radio(v-for="s in site" :label='s.name + ": " + s.ip' :value='s.name' )
                             v-layout(style='margin-top: -0.5%;')
                                 v-flex.py-6.pt-0.pb-0(xs8 sm8 md8)
                                     v-text-field(v-model="cliExam" label="Exam CLI Before Sending" readonly)
                                 v-flex.py-6.pt-0.pb-0(xs2 sm2 md2)
-                                    v-text-field(v-model="site" label="From" readonly)
+                                    v-text-field(v-model="siteSelected" label="From" readonly)
                             v-btn(color="primary" block @click="getDigInfo()") SEND
                             v-layout.pt-2(v-show="cliExecuted != false")
                                 v-flex.py-6.pt-0.pb-0(xs8 sm8 md8)
@@ -34,12 +40,25 @@ export default {
     mixins: [textFieldRules],
     data() {
         return {
-            site: 'TW',
+            site: [{
+                    name: 'HK',
+                    ip: '172.29.249.103',
+                },{
+                    name: 'TW',
+                    ip: '172.31.249.105',
+                },{
+                    name: 'PH',
+                    ip: '172.30.249.103',
+                }],
+            siteSelectedName: 'HK',
+            siteSelectedIP: '',
 
             destinationIP: '',
+            dnsServerIP: '',
 
             // output result
-            siteExecuted: 'TW',
+            siteNameExecuted: '',
+            siteIPExecuted: '',
             cliExecuted: false,
             digResult: '',
         }
@@ -49,24 +68,56 @@ export default {
         cliExam: function () {
             let cli = 'dig '
 
-            cli += this.destinationIP ? this.destinationIP : 'x.x.x.x'
+            cli += this.dnsServerIP ? this.dnsServerIP : '<DNS Server IP>'
+            cli += ' '
+            cli += this.destinationIP ? this.destinationIP : '<Destination FQDN>'
 
             return cli
         },
+
+        siteSelected: function () {
+            if (this.siteSelectedName != "") {
+                this.siteSelectedIP = this.site.find(function (v) {
+                    return v.name == this.siteSelectedName
+                }.bind(this)).ip
+                
+                return this.siteSelectedName + ': ' + this.siteSelectedIP
+            }
+            return ''
+        },
+
+        siteExecuted: function () {
+            if (this.siteNameExecuted != "") {
+                return this.siteNameExecuted + ': ' + this.siteIPExecuted
+            }
+            return ''
+        },
+
+        
     },
     methods: {
         getDigInfo: function() {
+            if (! this.validateForm()) {
+                this.$store.dispatch(
+                    'global/showSnackbarError',
+                    'Inputs Errors.'
+                )
+
+                return
+            }
             this.$store.dispatch('global/startLoading')
             this.$store
                 .dispatch('dig/getDigInfo', {
-                    // site: this.site,
+                    site: this.siteSelectedName,
                     destination_ip: this.destinationIP,
+                    dns_server: this.dnsServerIP,
                 })
                 .then(
                     function(result) {
                         this.digResult = result.data.result
                         this.cliExecuted = this.cliExam
-                        // this.siteExecuted = this.site
+                        this.siteNameExecuted = this.siteSelectedName
+                        this.siteIPExecuted = this.siteSelectedIP
                         this.$store.dispatch(
                             'global/showSnackbarSuccess',
                             'Success!'
@@ -83,6 +134,9 @@ export default {
                         this.$store.dispatch('global/finishLoading')
                     }.bind(this)
                 )
+        },
+        validateForm: function() {
+            return this.$refs.form.validate()
         },
     },
     mounted() {
